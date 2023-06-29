caa87c9e24ef61e0694a46212a5ffd5808f53100
KemyssonFernando <130847061+KemyssonFernando@users.noreply.github.com>
2023-06-29 13:44:50 -0300

    atividade 1 29/06/2023
    atividade feita !!

---
+++ atividade 1 29.06.2023.js
@@ -0,0 +1,16 @@
+const x = 5;
+const y = 8;
+
+const resultado = x === y;
+
+console.log(resultado);
+
+var x = 6;
+var y = '6';
+var resultado = x !== y;
+console.log (resultado);
+
+var x = '7';
+var y = 7;
+var resultado = x === y;
+console.log (resultado);
