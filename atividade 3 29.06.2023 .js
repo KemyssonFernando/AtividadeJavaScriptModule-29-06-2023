3acbf28a1b465f3bfee7afd85a09970627b0c221
KemyssonFernando <130847061+KemyssonFernando@users.noreply.github.com>
2023-06-29 14:17:00 -0300

    atividade 3 29/06/2023
    atividade  3 feita !!

---
+++ atividade 3 29.06.2023 .js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 5;
+
+console.log("O valor de  x é: " + (x += 5));
+
+console.log("O valor de  x é: " + (x -= 5));
+
+console.log("O valor de  x é: " + (x *= 5));
+
+console.log("O valor de  x é: " + (x /= 5));
+
+console.log("O valor de x é : " + (x %= 5))
